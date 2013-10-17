381fa8f59a5aa153f827f3979f077b7c08b886f2
zackattacknyu <zackattacknyu@gmail.com>
2013-10-16 17:49:03 -0700

    sample image reading added

--- src/start/SampleFileReferencing.java
+++ src/start/SampleFileReferencing.java
@@ -2,6 +2,8 @@ package start;
 
 import java.io.File;
 
+import samples.MyImageReader;
+
 
 //this illustrates how we will reference the JPG files in the sampleImages folder
 public class SampleFileReferencing {
@@ -16,5 +18,19 @@ public class SampleFileReferencing {
 		//here is an example use of the file
 		File someImage = new File(fileNameForArgument);
 		System.out.println("My File is " + someImage.length() + " bytes");
+		
+		//sample image output
+		int[][][] imageData = MyImageReader.readImageInto2DArray(fileNameForArgument);
+		for(int[][] channel: imageData){
+			for(int[] row: channel){
+				for(int point: row){
+					System.out.print(point + " ");
+				}
+				System.out.println();
+			}
+			System.out.println();
+			System.out.println();
+		}
+		System.out.println(MyImageReader.numchannels(fileNameForArgument) + " channel");
 	}
 }
