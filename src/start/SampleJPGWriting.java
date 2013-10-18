5e21a2569bbf01810de445bfbbc0a4143fb034c4
zackattacknyu <zackattacknyu@gmail.com>
2013-10-17 17:13:56 -0700

    added computation of image size

--- src/start/SampleJPGWriting.java
+++ src/start/SampleJPGWriting.java
@@ -26,10 +26,20 @@ public class SampleJPGWriting {
 		//this is the color to write
 		int lineColor = 100;
 		
+		//data about the image
+		int numChannels = 0;
+		int numPixelsWide = 0;
+		int numPixelsHigh = 0;
+		
 		//this is used to generate the image data
 		int[][][] imageData = MyImageReader.readImageInto2DArray(originalFileName);
+		numChannels = imageData.length;
 		for(int channelNo = 0; channelNo < imageData.length; channelNo++){
+			numPixelsHigh = imageData[channelNo].length;
 			for(int rowNum = 0; rowNum < imageData[channelNo].length; rowNum++){
+				
+				numPixelsWide = imageData[channelNo][rowNum].length;
+				
 				//this randomizes which lines are affected
 				if((Math.random()*probDenominator) < probNumerator){ 
 					for(int colNum = 0; colNum < imageData[channelNo][rowNum].length; colNum++){
@@ -39,6 +49,10 @@ public class SampleJPGWriting {
 				
 			}
 		}
+		
+		System.out.println("Num Channels = " + numChannels);
+		System.out.println("Num Pixels Wide = " + numPixelsWide);
+		System.out.println("Num Pixels High = " + numPixelsHigh);
 
 		//this writes the image using the image data
 		MyImageWriter.writeImage(originalFileName, newFileName, imageData);
